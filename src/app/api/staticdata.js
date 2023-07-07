55d4095ec567e7866a69155cac09be2d4b326fc4
yulinda ramadhana <yulinda.ramadhana98@gmail.com>
2023-07-07 18:17:27 +0700

    update getLocalData

--- src/app/api/staticdata.js
+++ src/app/api/staticdata.js
@@ -1,5 +1,8 @@
-export default async function getLocalData() {
-  const res = await fetch("http://localhost:3000/data/data.json");
+import spaceData from "../../../public/data/data.json";
 
-  return res.json();
+export default function getLocalData() {
+  // const res = await fetch("http://localhost:3000/data/data.json");
+
+  // return res.json();
+  return spaceData;
 }
