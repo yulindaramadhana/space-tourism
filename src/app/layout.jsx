fad1a0dd33ec1b4a6e92bac10530950b50e5749d
yulindaramadhana <yulinda.ramadhana98@gmail.com>
2024-09-19 21:51:55 +0700

    add gtag manager

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,19 +1,34 @@
-import { barlow, bellefair, barlow_condensed } from "@/utils/fonts";
-import "../styles/global.css";
-import { Navbar } from "@/components/header/navbar"
+import PageTitle from "@/components/body-content/page-title";
 
-export const meta = {
-    title: "Frontend Mentor | Space tourism website",
-    description: "Expoler the Space"
-}
-
-export default function RootLayout({ children }) {
+export default async function About() {
     return (
-        <html lang="en">
-            <body className={`${bellefair.variable} ${barlow_condensed.variable} ${barlow.variable} `}>
-                <Navbar />
-                <main>{children}</main>
-            </body>
-        </html>
+        <div
+            className={`custom-bg-img crew-page
+                            bg-crew-mobile 
+                            md:bg-crew-tablet 
+                            lg:bg-crew-desktop
+                            items-baseline
+                            h-screen 
+                            fixed `}
+        >
+            <div className="flex 
+                            flex-col 
+                            h-content-wrapper-height-sm
+                            md:h-content-wrapper-height-md
+                            fixed
+                            top-gap-top-content-sm
+                            md:top-gap-top-content-md
+                            lg:top-gap-top-content-lg
+                            px-horizontal-page-sm
+                            md:px-horizontal-page-md
+                            lg:px-horizontal-page-lg
+                            overflow-auto">
+                <PageTitle title={"CONNECT WITH US"} id={"04"} key={'page-about-title'} />
+                <div className="flex flex-col gap-y-2 items-start">
+                    <input type="text" id="input-subcribe" placeholder="email" className="text-baseColor-black p-4 rounded-sm" />
+                    <button id="btn-subscribe" className="p-4 bg-baseColor-black uppercase rounded-sm hover:bg-baseColor-black/80">Subscribe</button>
+                </div>
+            </div>
+        </div>
     )
 }
